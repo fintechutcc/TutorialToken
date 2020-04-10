d123095548eac864537cf7a0d8c2aad5a0aa5668
fintechutcc <48086552+fintechutcc@users.noreply.github.com>
2020-04-10 12:18:20 +0700

    Create TutorialToken.sol
    TutorialToken.sol

---
+++ TutorialToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.16;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract TutorialToken is ERC20 {
+    string public name = "TutorialToken";
+    string public symbol = "TT";
+    uint8 public decimals = 2;
+    uint public INITIAL_SUPPLY = 12000;
+
+    constructor() public {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
